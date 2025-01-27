be86377374d5d3dbdffda95822cd8bd7de314892
Asvapp <ansavaara7@gmail.com>
2025-01-27 19:16:53 +0200

    päivitetty config

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,62 @@
-import withPWA from 'next-pwa'
+import NextPWA from 'next-pwa';
 
-/** @type {import('next').NextConfig} */
-const nextConfig = withPWA({
+const withPWA = NextPWA({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development'
-})({
-  reactStrictMode: true,
-})
+  disable: process.env.NODE_ENV === 'development',
+  buildExcludes: [
+    /app-build-manifest\.json$/,
+    /middleware-manifest\.json$/,
+  ],
+  runtimeCaching: [
+    {
+      urlPattern: /_next\/.*/,
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'next-assets',
+        expiration: {
+          maxEntries: 50,
+          maxAgeSeconds: 24 * 60 * 60,
+        },
+      },
+    },
+    {
+      urlPattern: /\/api\/.*/,
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'api-cache',
+        expiration: {
+          maxEntries: 50,
+          maxAgeSeconds: 60 * 60,
+        },
+      },
+    },
+    {
+      urlPattern: /\.(?:png|gif|jpg|jpeg|svg)$/,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'images',
+        expiration: {
+          maxEntries: 20,
+          maxAgeSeconds: 7 * 24 * 60 * 60,
+        },
+      },
+    }
+  ],
+});
 
-export default nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  rewrites: async () => {
+    return [
+      {
+        source: '/manifest.json',
+        destination: '/api/manifest',
+      },
+    ];
+  },
+};
+
+export default withPWA(nextConfig);
