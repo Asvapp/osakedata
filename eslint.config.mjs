d922c7996b796e01588a9212e2ca21f71ec3a6ac
Asvapp <ansavaara7@gmail.com>
2025-01-27 18:53:01 +0200

    Add PWA config and eslint ignores

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,9 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: ['public/sw.js', 'public/workbox-*.js']
+  }
 ];
 
 export default eslintConfig;
